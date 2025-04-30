83c06def0b6ef9cbf9fb1a39ab467b05ff39ef78
Daniel Finnerty <danielfinnerty@hotmail.com>
2025-04-30 19:11:31 +0100

    Updated accounts.py file, added code comments

--- analysis.py
+++ analysis.py
@@ -16,12 +16,12 @@ import seaborn as sns
 # specify filename of data source
 FILENAME = "iris.data"
 
+# open iris.data file
 with open(FILENAME, "r+") as f:
-    # import data from file
+    # import data from file and call it 'data'
     data = pd.read_csv(FILENAME, header=None)
-    # adding columns to data
+    # specifying column names to 'data' file
     data.columns = [ "sepal length", "sepal width", "petal length", "petal width", "class"] 
-
 
 # specify new file name
 NEW_FILENAME = "summary.txt"
@@ -29,10 +29,11 @@ NEW_FILENAME = "summary.txt"
 with open(NEW_FILENAME, "w") as n:
     # create high level overview
     stats = data.describe()
-    # write data overview to new file, as a string
+    # write overview to new file, as a string
     n.write(stats.to_string())
-    # write measurements data frame to same file as string
+    # write data frame to same file as string
     n.write('\n' + '\n' + data.to_string(header=True, index=True))
+
 
 
 # Task 3.2
@@ -40,9 +41,13 @@ with open(NEW_FILENAME, "w") as n:
 # Function for creating histogram plots
 def hist_plot(variables):
     for variable in variables:
+        # create histogram plot for each variable
         sns.histplot(data, x = variable, hue = 'class', bins = 20)
+        # add title of given variable
         plt.title(f'{variable} histogram plot')
+        # save .png file of histogram
         plt.savefig(f'{variable} hist.png')
+        # close function to allow next variable
         plt.close()
 
 # Create list of variables for plotting
@@ -52,25 +57,29 @@ variables = ['sepal length', 'sepal width', 'petal length', 'petal width']
 hist_plot(variables)
 
 
+
 # Task 3.3
 
+# create pairplot
 sns.pairplot(data, hue = 'class', diag_kind = 'hist', diag_kws = {'bins' : 20})
+# save pairplot as .png file
 plt.savefig('iris pairplot.png')
 plt.close()
-# source for bins solution:https://stackoverflow.com/questions/59696426/how-to-change-the-number-of-bins-in-seaborns-pairplot-function
+
 
 
 # Task  3.4
-# source for regression line in pairplot: https://stackoverflow.com/questions/50722972/change-the-regression-line-colour-of-seaborns-pairplot
-iris_pair_reg = sns.pairplot(data, hue='class', diag_kind = 'hist', diag_kws = {'bins' : 20}, corner=True)
-#plt.show()
 
-# single line: https://stackoverflow.com/questions/76217544/how-to-fit-regression-lines-on-each-non-diagonal-segment-of-a-pairplot-while-re
+# create masked pairplot
+iris_pair_reg = sns.pairplot(data, hue='class', diag_kind = 'hist', diag_kws = {'bins' : 20}, corner=True)
+
+# create a regression lines for each plot, but hiding the scatter points
 def regline(x, y, **kwargs):
     sns.regplot(data=kwargs['data'], x=x.name, y=y.name, scatter=False, color=kwargs['color'])
 
+# merge the regression lines to the pairplot
 iris_pair_reg.map_offdiag(regline, color='red', data=data)
-plt.savefig('pairplot & reg line.png')
+plt.savefig('pairplot & regline.png')
 plt.close()
 
 
